2528d62bbf7a2fbfbc18be483b555a4a9e57ddc1
akjaya <putulakjaya@gmail.com>
2020-02-14 16:17:30 +0530

    first

---
+++ TRY3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRY3.MvcApplication" Language="C#" %>
---
+++ TRY3/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace TRY3.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
